6ac846ea2daabc458ad5e816061198e0f6cb049a
LukeTownsendJCU <luke.townsend@my.jcu.edu.au>
2019-08-29 12:26:43 +1000

    initial commit

---
+++ lists_warmup.py
@@ -0,0 +1,12 @@
+"""Change the first element of numbers to "ten"
+Change the last element of numbers to 1
+Get all the elements from numbers except the first two
+Check if 9 is an element of numbers"""
+
+
+def main():
+
+    numbers = [3, 1, 4, 1, 5, 9, 2]
+
+
+main()
